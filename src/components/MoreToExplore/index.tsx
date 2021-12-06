69886ed4d5d28ff155e802ce3bcfafa49aa3fe37
Manitajs <manita.paudel@ombryo.com>
2021-12-06 15:30:13 +0545

    Add axios, fetch data from JSON placeholder

--- src/components/MoreToExplore/index.tsx
+++ src/components/MoreToExplore/index.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+
+import axios from "../../utils/axios";
 import ExploreCard from "../ExploreCard";
 import { H3 } from "../Typography";
 
@@ -10,31 +13,52 @@ const explores = [
     title: "12 incredible hikes for hiking-obsessed families.",
     href: "/",
     imgSrc: "sea-sight.jpg",
-    alt:"Two peeople looking at the sea from a cliff"
+    alt: "Two people looking at the sea from a cliff",
   },
   {
     title:
       "Ice cream, zoo time, and the beach—the best weekend in San Diego ever",
     href: "/",
-    imgSrc: "horse-back-riding.jpg",
-    alt: "An ice-cream sandwich with sprinkles"
+    imgSrc: "ice-cream-sandwich.jpg",
+    alt: "An ice-cream sandwich with sprinkles",
   },
   {
     title: "21 film and TV locations you can actually visit",
     href: "/",
-    imgSrc: "ice-cream-sandwich.jpg",
-    alt:"People riding horse-back in meadow"
+    imgSrc: "horse-back-riding.jpg",
+    alt: "People riding horse-back in meadow",
   },
 ];
 
 const MoreToExplore: React.FC<MoreToExploreProps> = ({ className = "" }) => {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const resp: any = await axios.get("/posts");
+        // console.log("Data", resp.data);
+        setData(resp.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  console.log("Data", data);
   return (
     <div className={`bg-pink-100 p-4 rounded-md h-auto w-full ${className}`}>
       <H3>More to Explore</H3>
       <div className="container mx-auto mt-2 grid grid-cols-3 gap-3">
-      {explores.map(({title, href, imgSrc, alt}) => (
-        <ExploreCard key={imgSrc} title={title} href={href} imgSrc={imgSrc} alt={alt}/>
-      ))}
+        {explores.map(({ title, href, imgSrc, alt }) => (
+          <ExploreCard
+            key={imgSrc}
+            title={title}
+            href={href}
+            imgSrc={imgSrc}
+            alt={alt}
+          />
+        ))}
       </div>
     </div>
   );
