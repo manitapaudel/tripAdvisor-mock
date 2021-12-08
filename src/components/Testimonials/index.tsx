00d3edc13a3b0a687a14402d600807d614764cfc
Manitajs <manita.paudel@ombryo.com>
2021-12-08 15:50:08 +0545

    Create components for icons, add the photos, title from the photos api to the testimonials section

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DoubleQuotesIcon from "../../assets/icons/DoubleQuotesIcon";
 import axios from "../../utils/axios";
 import { H3 } from "../Typography";
 
@@ -11,7 +12,7 @@ const Testimonials: React.FC<TestimonialProps> = ({ className = "" }) => {
   React.useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const resp: any = await axios.get("/posts?_limit=6");
+        const resp: any = await axios.get("/photos?_limit=6");
         // console.log("Data", resp.data);
         setData(resp.data);
       } catch (error) {
@@ -25,12 +26,18 @@ const Testimonials: React.FC<TestimonialProps> = ({ className = "" }) => {
 
   return (
     <div className={`bg-green-100 p-3 ${className}`}>
-      <div>
+      <div className="container mx-auto">
         <H3>Testimonials</H3>
-        <section className="bg-white grid grid-cols-3 gap-4">
-        {data.map(({ title, body }) => (
-          <section className="">{title}</section>
-        ))}
+        <section className="grid grid-cols-4 gap-5 mt-2">
+          {data.map(({ title, url, body }) => (
+            <section className="bg-white p-3 rounded-md">
+              <img src={url} height="200px" width="auto" alt="images" />
+              <span className="pl-3 font-serif">
+                <DoubleQuotesIcon />
+                <i>{title}</i>
+              </span>
+            </section>
+          ))}
         </section>
       </div>
     </div>
